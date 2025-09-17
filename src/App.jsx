8db46f77311a27d288373db980a38a8991ea4598
TruongVoKy2k5 <chuhonganh0701@gmail.com>
2025-09-17 11:08:50 +0700

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+import todoInput from "/ToDo/todoInput.jsx";
+
+function App() {
+  const [count, setCount] = useState(0)
+  const [todo, setTodo] = useState([]);
+
+  function addTodo(){
+
+  }
+  return;
+}
+
+export default App
---
+++ src/ToDo/todoInput.jsx
@@ -0,0 +1,16 @@
+import {useState} from "react";
+export default function todoInput() {
+    const [text, setText] = useState("");
+    function handleOnchangeInput(event){
+        setText(event.target.value);
+    }
+    function handleFormSubmit(event){
+        event.preventDefault();
+    }
+    return (
+        <form onSubmit={handleFormSubmit}> 
+            <input onChange={handleOnchangeInput} placeholder="What you want to do ?" value={text}></input>
+            <button  type="submit">Add</button>
+        </form>
+    )
+}
